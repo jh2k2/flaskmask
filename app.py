dd401cb6820e93bece6dcf6f5e5cce40bac4c3ce
jh2k2 <jerryhsieh20020220@gmail.com>
2021-01-02 14:33:04 +0800

    added aboutus routing

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, render_template
 from predictor import predictor
 
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-	if request.method == 'POST':
-		file = request.files['file'].read()
-		result = predictor.predict(file)
-		return result
-	else:
-		return render_template('index.html')
+    if request.method == 'POST':
+        file = request.files['file'].read()
+        result = predictor.predict(file)
+        return result
+    else:
+        return render_template('index.html')
 
-if __name__ == '__main__':
-	app.run()
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+if name == 'main':
+    app.run(debug=True)
